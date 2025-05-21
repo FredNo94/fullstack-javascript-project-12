be1029d8bbf0cc02bcc75385561c7d6a85689d02
FredNo94 <galenkov9@gmail.com>
2025-05-21 23:47:30 +0300

    Fix issue by lint and linter

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,46 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  js.configs.recommended,
+  {
+    ignores: ["dist/**/*.js", "node_modules/**"]
+  },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        process: "readonly",
+        __REACT_DEVTOOLS_GLOBAL_HOOK__: "readonly",
+        setImmediate: "readonly"
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  },
+  {
+    plugins: {
+      react: pluginReact
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "no-undef": "warn",
+      "no-unreachable": "warn",
+      "no-empty": "warn",
+      "no-cond-assign": "warn",
+      "no-prototype-builtins": "off",
+      "no-control-regex": "off"
+    }
+  }
+];
